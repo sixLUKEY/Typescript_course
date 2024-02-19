94bf4970105ee4892d7bd880ebe93f4a1031bdce
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-19 15:07:22 +0200

    Overriding properties + protected modifier completed

--- Section_5/app.ts
+++ Section_5/app.ts
@@ -1,13 +1,13 @@
 class Department {
-   name: string;
-  private employees: string[] = [];
+  //   private name: string;
+  protected employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: number, public name: string) {
+    // this.name = n;
   }
 
   describe() {
-    return `Department: ${this.name}`;
+    return `Department: (${this.id}) ${this.name}`;
   }
 
   addEmployee(employee: string) {
@@ -20,9 +20,38 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
+class ITDepartment extends Department {
+  constructor(id: number, public admins: string[]) {
+    super(id, "IT");
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: number, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+        return
+    }
+    this.employees.push(name)
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printreports() {
+    return this.reports;
+  }
+}
+
+const accounting = new AccountingDepartment(25, ["General Ledger"]);
+const infos = new ITDepartment(28, ["Max"]);
 
 accounting.addEmployee("Phineas");
 accounting.addEmployee("Kobus");
 accounting.describe();
 accounting.printEmployeeInformation();
+accounting.addReport("Balance Sheet");
