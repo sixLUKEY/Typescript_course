5d364349b5a876e9bc87f2fd6ceba664cdf57213
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-26 15:04:16 +0200

    Generic functions completed

--- Section_7/app.js
+++ Section_7/app.js
@@ -1,16 +1,42 @@
 "use strict";
 function merge(objA, objB) {
-    //@ts-ignore
     return Object.assign(objA, objB);
 }
 let objectA = {
     name: "Luke",
     age: 23,
-    regions: ['ZA']
+    regions: ["ZA"],
 };
 let objectB = {
     name: "AK",
     age: 47,
 };
 let mergedObject = merge(objectA, objectB);
-console.log(mergedObject.name);
+console.log(mergedObject);
+let anotherMergedObject = merge({ name: "Felix", size: 35 }, { age: 300 });
+console.log(anotherMergedObject);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value!";
+    if (element.length === 1) {
+        descriptionText = "Got 1 character.";
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} characters.`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("Goodday"));
+function extractAndConvert(obj, key) {
+    return obj[key];
+}
+let newObj = {
+    name: "New Object",
+    age: 22,
+    length: 25,
+};
+let anotherNewObj = {
+    name: "Another One",
+    age: 30,
+};
+console.log(extractAndConvert(newObj, "age"));
+console.log(extractAndConvert(anotherNewObj, "length"));
--- Section_7/app.ts
+++ Section_7/app.ts
@@ -4,15 +4,14 @@ interface Objectibles {
   regions?: string[];
 }
 
-function merge<T, U>(objA: T, objB: U) {
-    //@ts-ignore
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 let objectA: Objectibles = {
   name: "Luke",
   age: 23,
-  regions: ['ZA']
+  regions: ["ZA"],
 };
 
 let objectB: Objectibles = {
@@ -21,4 +20,61 @@ let objectB: Objectibles = {
 };
 
 let mergedObject = merge(objectA, objectB);
-console.log(mergedObject.name);
+console.log(mergedObject);
+
+let anotherMergedObject = merge({ name: "Felix", size: 35 }, { age: 300 });
+console.log(anotherMergedObject);
+
+// Extending with Generic Functions
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value!";
+
+  if (element.length === 1) {
+    descriptionText = "Got 1 character.";
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} characters.`;
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Goodday"));
+
+// Using Keyof Constraint
+interface TestObject {
+  name: string;
+  age: number;
+  length?: number;
+}
+
+function extractAndConvert<T extends TestObject, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+let newObj: TestObject = {
+  name: "New Object",
+  age: 22,
+  length: 25,
+};
+
+let anotherNewObj: TestObject = {
+  name: "Another One",
+  age: 30,
+};
+
+let lastObj = {
+  name: "pilib",
+  age: 80,
+  drive: 60,
+};
+
+console.log(extractAndConvert(newObj, "age"));
+console.log(extractAndConvert(anotherNewObj, "length"));
+console.log(extractAndConvert(lastObj, 'drive'));
