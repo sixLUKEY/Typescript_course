df7aa5c426433c1f5c6d17d528c31e02a5966968
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-26 15:56:54 +0200

    Generic classes complete

---
+++ Section_7/generic-functions.js
@@ -0,0 +1,48 @@
+"use strict";
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+let objectA = {
+    name: "Luke",
+    age: 23,
+    regions: ["ZA"],
+};
+let objectB = {
+    name: "AK",
+    age: 47,
+};
+let mergedObject = merge(objectA, objectB);
+console.log(mergedObject);
+let anotherMergedObject = merge({ name: "Felix", size: 35 }, { age: 300 });
+console.log(anotherMergedObject);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value!";
+    if (element.length === 1) {
+        descriptionText = "Got 1 character.";
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} characters.`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("Goodday"));
+function extractAndConvert(obj, key) {
+    return obj[key];
+}
+let newObj = {
+    name: "New Object",
+    age: 22,
+    length: 25,
+};
+let anotherNewObj = {
+    name: "Another One",
+    age: 30,
+};
+let lastObj = {
+    name: "pilib",
+    age: 80,
+    drive: 60,
+};
+console.log(extractAndConvert(newObj, "age"));
+console.log(extractAndConvert(anotherNewObj, "length"));
+console.log(extractAndConvert(lastObj, 'drive'));
