5801a91bff203b435af87c795bf35592d33f6312
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-28 15:10:26 +0200

    More useful decorators

--- Section_8/app.ts
+++ Section_8/app.ts
@@ -1,3 +1,22 @@
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
@@ -7,4 +26,4 @@ class Person {
 }
 
 const pers = new Person();
-console.log(pers)
+console.log(pers);
