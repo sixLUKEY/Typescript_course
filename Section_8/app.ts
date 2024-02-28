8d6ec3a3a8498aff1bfbe4117ea9d4654a6ea682
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-28 15:35:58 +0200

    Property decorators

--- Section_8/app.ts
+++ Section_8/app.ts
@@ -1,30 +1,25 @@
-function Logger(logString: string) {
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
+function Logger(target: any, propertyName: string | symbol) {
+  console.log("Property Decorator!!");
+  console.log(target, propertyName);
 }
 
-function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
+class Product {
+  @Logger
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
     }
-  };
-}
+  }
 
-@Logger('Logging...')
-@WithTemplate("<h1>My Person Object</h1>", "app")
-class Person {
-  name = "Max";
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
 
-  constructor() {
-    console.log("Creating person object...");
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
   }
 }
-
-const pers = new Person();
-console.log(pers);
