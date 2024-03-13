fedea1a74939d96dc5c0e94d58f3a130c7198635
sixLUKEY <marcoslukerojo@gmail.com>
2024-03-13 14:25:08 +0200

    Section 12 completed

--- Section_12/src/app.ts
+++ Section_12/src/app.ts
@@ -1,3 +1,33 @@
-import _ from "lodash";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product.model";
+import { validate } from "class-validator";
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  {
+    title: "carpet",
+    price: 199,
+  },
+  {
+    title: "tree",
+    price: 300,
+  },
+];
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+
+// const p1 = new Product("A book", 250);
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
