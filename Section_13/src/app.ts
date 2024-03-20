4e6817610660c3086efaab821c6ffc8696b7b3da
sixLUKEY <marcoslukerojo@gmail.com>
2024-03-20 12:25:33 +0200

    fetch data added using Axios

--- Section_13/src/app.ts
+++ Section_13/src/app.ts
@@ -1,5 +1,14 @@
+import axios from "axios";
+
 const form = document.querySelector("form") as HTMLFormElement;
 const addresInput = document.getElementById("address") as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyCxpgUgi5d9teV1c36kwfB8AjlU6ZLLKsU";
+
+type GeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -7,6 +16,23 @@ function searchAddressHandler(event: Event) {
   const enteredAddress = addresInput.value;
 
   // add google api
+  axios
+    .get<GeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err: Error) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
